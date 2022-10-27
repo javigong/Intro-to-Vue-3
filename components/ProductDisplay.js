98d666596a7e8679609f3f2419921664b6d80fcc
Javier Gongora <42308135+javigong@users.noreply.github.com>
2022-10-27 13:41:43 -0700

    Change position for review list and form

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -38,9 +38,9 @@ app.component("product-display", {
           v-on:click="addToCart">
           Add to Cart
         </button>
-      <review-list v-if="reviews.length" :reviews="reviews"></review-list>
-      <review-form @review-submitted="addReview"></review-form>
-      </div>
+        </div>
+        <review-list v-if="reviews.length" :reviews="reviews"></review-list>
+        <review-form @review-submitted="addReview"></review-form>
     </div>
   </div>`,
   data() {
